e50cfee5e6f47b187fc0e2f7d025edec74cda059
mohammad.ashikur <mohammad.ashikur@neural-semiconductor.com>
2023-04-27 15:53:40 +0600

    Introducing Traits and implementation of traits for different Types

--- src/area.rs
+++ src/area.rs
@@ -2,15 +2,30 @@ pub fn test_external() -> &'static str {
 			"<==== External Mod area.rs Has Been Called. ======>"
 }
 
+pub trait Area {
+	fn area(&self) -> f64;
+}
+
 pub struct Circle {
 	pub x: f64,
 	pub y: f64,
 	pub radius: f64
 }
 
-impl Circle {
-	pub fn area(&self) -> f64 {
+pub struct Square {
+	pub x: f64,
+	pub y: f64
+}
+
+impl Area for Circle {
+	fn area(&self) -> f64 {
 		std::f64::consts::PI * (self.radius * self.radius)
+	}
+}
+
+impl Area for Square {
+	fn area(&self) -> f64 {
+		self.x * self.y
 	}
 }
 
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ fn main() {
 		y: 5.0,
 		radius: 10.0
 	};
-	let area_of_circle = circle.area();
+	let square = area::Square {
+		x : 5.0,
+		y: 5.0
+	};
+	let area_of_circle = area::Area::area(&circle);
 	println!("area of a circle with radius {0} is: {1}", circle.radius, area_of_circle);
+	
+	let area_of_square = area::Area::area(&square);
+	println!("area of a square with x: {0} and y: {1} is: {2}", square.x, square.y, area_of_square);
 }
