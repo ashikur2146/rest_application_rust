53f155db6cbfa1e946817e7a2960f9096317c367
mohammad.ashikur <mohammad.ashikur@neural-semiconductor.com>
2023-04-27 12:17:27 +0600

    test commit with addition function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    let a = 20;
+    let b = 20;
+    let c = addition(a, b);
+    println!("sum of {0} and {1} is {2}. ", a, b, c);
+}
+
+fn addition(a: i32, b: i32) -> i32 {
+	a + b
 }
